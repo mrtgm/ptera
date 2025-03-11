ae6a72be41625f83ba2681e4899600f31cddec98
Morio <xtarako@gmail.com>
2025-03-11 16:54:32 +0900

    feat: delete actions & terraform

--- terraform/modules/monitoring/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sns_topic_arn" {
-  description = "ARN of the SNS topic for alerts"
-  value       = aws_sns_topic.alerts.arn
-}
-
-output "monthly_budget_id" {
-  description = "ID of the monthly budget"
-  value       = aws_budgets_budget.monthly.id
-}
