62eeec4eec8aa45758f09e0798158ab7cd1e6bf3
Morio <xtarako@gmail.com>
2025-03-17 12:06:07 +0900

    chore: console.log

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -4,6 +4,18 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
 	const authCookie = request.cookies.get("ptera-auth");
 
+	const header = request.headers.get("cookie");
+
+	const headerKeys = request.headers.keys();
+	const headerValues = request.headers.values();
+
+	console.log(headerKeys);
+	console.log(headerValues);
+
+	console.log(header);
+	console.log(request.nextUrl.pathname);
+	console.log(authCookie);
+
 	if (request.nextUrl.pathname.startsWith("/dashboard")) {
 		if (!authCookie) {
 			return NextResponse.redirect(new URL("/login", request.url));
